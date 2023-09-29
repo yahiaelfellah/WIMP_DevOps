73fab19ee3b5b7529d117b564677bedb31133b0d
YahiaFellah <33671463+yahiaelfellah@users.noreply.github.com>
2023-09-28 21:40:21 -0400

    update gateway  :  -update routes , .env

--- gateway/routes/routes.js
+++ gateway/routes/routes.js
@@ -1,10 +1,15 @@
 const API_PREFIX = "api/v1";
-
+const USER_URL = process.env.USER_URL || "http://[::1]:3001"; // Replace with your actual environment variable name
+const FLOW_URL = process.env.FLOW_URL || "http://[::1]:3002";
+const DEVICE_URL = process.env.FLOW_URL || "http://[::1]:3003";
+const DEPARTEMENT_URL = process.env.FLOW_URL || "http://[::1]:3004";
+const CAMERA_URL = process.env.CAMERA || "http://[::1]:3006";
+const WEMO_URL = process.env.WEMO || "http://[::1]:3007";
 exports.routes = [
   {
     url: `/${API_PREFIX}/auth`,
     proxy: {
-      target: "http://[::1]:3001/auth",
+      target: `${USER_URL}/auth`,
       changeOrigin: true,
       pathRewrite: {
         [`^/${API_PREFIX}/auth`]: "",
@@ -20,7 +25,7 @@ exports.routes = [
       max: 5,
     },
     proxy: {
-      target: "http://[::1]:3001/refresh",
+      target: `${USER_URL}/refresh`,
       changeOrigin: true,
       timeout: 3000,
       pathRewrite: {
@@ -31,7 +36,7 @@ exports.routes = [
   {
     url: `/${API_PREFIX}/admin`,
     proxy: {
-      target: "http://[::1]:3001/admin",
+      target: `${USER_URL}/admin`,
       changeOrigin: true,
       pathRewrite: {
         [`^/${API_PREFIX}/admin`]: "",
@@ -40,10 +45,9 @@ exports.routes = [
   },
   {
     url: `/${API_PREFIX}/users`,
-    authenticationRequired: true, // if a user needs to be authenticationRequiredenticated for accessing this endpoint
+    authenticationRequired: true,
     proxy: {
-      // contains information about the target to which the request should be redirected
-      target: "http://[::1]:3001/users",
+      target: `${USER_URL}/users`,
       changeOrigin: true,
       pathRewrite: {
         [`^/${API_PREFIX}/users`]: "",
@@ -52,10 +56,9 @@ exports.routes = [
   },
   {
     url: `/${API_PREFIX}/departement`,
-    authenticationRequired: true, // if a user needs to be authenticationRequiredenticated for accessing this endpoint
+    authenticationRequired: true,
     proxy: {
-      // contains information about the target to which the request should be redirected
-      target: "http://[::1]:3001/departement",
+      target: `${DEPARTEMENT_URL}/departement`,
       changeOrigin: true,
       pathRewrite: {
         [`^/${API_PREFIX}/departement`]: "",
@@ -64,10 +67,9 @@ exports.routes = [
   },
   {
     url: `/${API_PREFIX}/flow`,
-    authenticationRequired: true, // if a user needs to be authenticated for accessing this endpoint
+    authenticationRequired: true,
     proxy: {
-      // contains information about the target to which the request should be redirected
-      target: "http://[::1]:8000/flow",
+      target: `${FLOW_URL}/flow`,
       changeOrigin: true,
       pathRewrite: {
         [`^/${API_PREFIX}/flow`]: "",
@@ -76,26 +78,36 @@ exports.routes = [
   },
   {
     url: `/${API_PREFIX}/devices`,
-    authenticationRequired: true, // if a user needs to be authenticated for accessing this endpoint
+    authenticationRequired: true,
     proxy: {
-      // contains information about the target to which the request should be redirected
-      target: "http://[::1]:3006/devices",
+      target: `${DEVICE_URL}/devices`,
       changeOrigin: true,
       pathRewrite: {
         [`^/${API_PREFIX}/devices`]: "",
       },
     },
   },
-  // {
-  //     url:`/${API_PREFIX}/flows`,
-  //     authenticationRequired:true, // if a user needs to be authenticated for accessing this endpoint
-  //     proxy: {
-  //         // contains information about the target to which the request should be redirected
-  //         target:"http://[::1]:8000/flows",
-  //         changeOrigin : true,
-  //         pathRewrite: {
-  //             [`^/${API_PREFIX}/flows`]: '',
-  //         },
-  //     }
-  // },
+  {
+    url: `/${API_PREFIX}/camera`,
+    authenticationRequired: true,
+    proxy: {
+      target: `${CAMERA_URL}/camera`,
+      changeOrigin: true,
+      pathRewrite: {
+        [`^/${API_PREFIX}/camera`]: "",
+      },
+    },
+  },
+  {
+    url: `/${API_PREFIX}/wemo`,
+    authenticationRequired: true,
+    proxy: {
+      target: `${WEMO_URL}/wemo`,
+      changeOrigin: true,
+      pathRewrite: {
+        [`^/${API_PREFIX}/wemo`]: "",
+      },
+    },
+  },
+  // Add more routes as needed
 ];
